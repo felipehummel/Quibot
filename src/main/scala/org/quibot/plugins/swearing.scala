f35b5f95eeebae328ebc36565bafc2917a5fd4c2
Felipe Hummel <felipehummel@gmail.com>
2011-11-04 18:14:00 -0400

    Adding more stuff to swearing and replacing sayTo with reply method

--- src/main/scala/org/quibot/plugins/swearing.scala
+++ src/main/scala/org/quibot/plugins/swearing.scala
@@ -2,20 +2,29 @@ package org.quibot.plugins
 
 import org.quibot._
 
-case class SwearingPlugin extends QuiBotPlugin {
+case class SwearingPlugin extends QuiBotPlugin with MessageRandomizer {
+    val swearingMessages = Array("manere seu linguajar robin, você ainda é um menino!", "teu cu","vai se fuderem", "VOCES NAUM MERECEM FALAR COM MEU BEBE", "seu madeerfãquer! seila como escreve", "pega essa porra desse arquivo e enfia no cu",
+        "vou zerar tua conta seu fdp", "Seu saldo é de R$ 37,23 ... ... ... ... POBRE!", "pega na minha e balança", "vai tomar bem no meio da berada do seu cu", "I like turtles", "vou comer o robin hoje", "vou comer a tia do batimaaaa", "teu cu é meu", "e a mãe vai bem?")
+
     respondTo("que time é teu\\? *$") { msg =>
-        sayTo(msg.channel, msg.user.nick.nickname, "bateu na trave e entrou no teu")
+        reply(msg, "bateu na trave e entrou no teu")
     }    
-    respondTo("(foda|caralho|fuder|fuck| cu)( +|$)") { msg =>
-        sayTo(msg.channel, msg.user.nick.nickname, "manere seu linguajar robin, você ainda é um menino!")
+    respondTo("(cuzao|vai pra merda|vai se fuder|te fode|vai tomar no cu|teu cu|chupa|gay|baitola|bixa|foda|caralho|fuder|fuck| cu|bosta)( +|$)") { msg =>
+        reply(msg, randomize(swearingMessages))
     }
     respondTo("porra+ *$") { msg =>
-        sayTo(msg.channel, msg.user.nick.nickname, "porra é o cu da cachorra")
+        reply(msg, "porra é o cu da cachorra")
     }
     respondTo("cachorra morreu teu cu (é|eh|e) meu *$") { msg =>
-        sayTo(msg.channel, msg.user.nick.nickname, "ahhh meu cu é seu? Estás insinuando que sou viado?")
+        reply(msg, "ahhh meu cu é seu? Estás insinuando que sou viado?")
     }
     respondTo("sim,? tu [ée]s viado!? *$") { msg =>
-        sayTo(msg.channel, msg.user.nick.nickname, "viado é rolimã, como tu e tua irmã")
+        reply(msg, "viado é rolimã, como tu e tua irmã")
+    }
+    respondTo("viado *$") { msg =>
+        reply(msg, "viado é rolimã, como tu e tua irmã")
+    }
+    respondTo(" *d[ea] onde vo?c[eê]? tirou is[st]o? *$") { msg =>
+        reply(msg, "do cu é claro robin! De onde mais seria?")
     }
 }
