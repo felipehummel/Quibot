e4eda32064f8005ee25a801abd47258c243a2b1e
Felipe Hummel <felipehummel@gmail.com>
2011-11-04 18:42:08 -0400

    Added MessageRandomizer helper trait
    CLICommands trait now tracks error stream also

--- src/main/scala/org/quibot/helpers.scala
+++ src/main/scala/org/quibot/helpers.scala
@@ -8,24 +8,28 @@ trait CLICommands {
     val runTime = Runtime.getRuntime
 
     /**
-    * from: http://code.google.com/p/scala-utilities/source/browse/trunk/helper.scala
+    * originally based on: http://code.google.com/p/scala-utilities/source/browse/trunk/helper.scala
     */
     def exec (cmd : String) : (Int, List[String]) = {
-        val process = if (currDir.isDefined) runTime.exec (cmd, null, currDir.getOrElse(new File("."))) else runTime.exec(cmd)
+        val builder = new ProcessBuilder(cmd.split(" "):_*)
+        builder.directory(currDir.getOrElse(new File(".")))
+        builder.redirectErrorStream(true)
+        val process = builder.start()
         val resultBuffer = new BufferedReader(new InputStreamReader(process.getInputStream))
         var line : String = null
         var lineList : List[String] = Nil
-
         do {
             line = resultBuffer.readLine
-            if (line != null) {
+            if (line != null) 
                 lineList = line :: lineList
-            }
         } while (line != null)
-
         process.waitFor
         resultBuffer.close
-
         (process.exitValue, lineList.reverse)
     }
+}
+
+trait MessageRandomizer {
+    val rand = new scala.util.Random
+    def randomize(msgs: IndexedSeq[String]) = msgs((rand.nextFloat * msgs.size).toInt)
 }
