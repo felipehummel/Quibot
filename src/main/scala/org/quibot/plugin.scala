230a9f8c46eca8e6d3e8975fa9aa3acf895d8e58
Felipe Hummel <felipehummel@gmail.com>
2011-11-07 18:19:58 -0400

    Adding some sugar methods to QuiBotPlugin

--- src/main/scala/org/quibot/plugin.scala
+++ src/main/scala/org/quibot/plugin.scala
@@ -14,11 +14,14 @@ trait QuiBotPlugin {
         commands += Command(regex)(f)  
     }
 
+    def say(channel: Channel, msgs: List[String]) : Unit = say(channel, msgs:_*)
     def say(channel: Channel, msgs: String*) = bot map { quibot => quibot say (channel, msgs:_*) }
+    def sayTo(channel: Channel, userNick: String, msgs: List[String]) : Unit = sayTo(channel, userNick, msgs:_*)
     def sayTo(channel: Channel, userNick: String, msgs: String*) = bot map { quibot =>  quibot sayTo(channel, userNick, msgs:_*) }
     def sayAllChannels(msgs: String*) = bot map { quibot => 
 	    quibot.joinedChannels.foreach { c => quibot say(c, msgs:_*) } 
 	}
+	def reply(msg: MatchedMessage, replies: List[String]) : Unit = reply(msg, replies:_*)
 	def reply(msg: MatchedMessage, replies: String*) = bot map { quibot =>
 		sayTo(msg.channel, msg.user.nick.nickname, replies:_*)
 	}
